4bbd7755a5300b9e2296b2da6a4e5d60bdcaa8ed
sansanguangguang <3370057926@qq.com>
2022-11-09 19:19:46 +0800

    这是第五周的作业

--- ch4/code/js/index.js
+++ ch4/code/js/index.js
@@ -32,9 +32,33 @@ $(function() {
         controlsColor: "#d7d7d7", //普通控制按钮的颜色
         controlsCurrentColor: "#00ff00", //当前控制按钮的颜色
     });
+    // 电子书轮播2
+    $('#ebooks-banner2').tyslide({
+        boxh: 223, //盒子的高度
+        w: 332, //盒子的宽度
+        h: 223, //图片的高度
+        isShow: true, //是否显示控制器
+        isShowBtn: true, //是否显示左右按钮
+        controltop: 10, //控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW: 20, //控制按钮宽度
+        controlsH: 3, //控制按钮高度
+        controlsColor: "#d7d7d7", //普通控制按钮的颜色
+        controlsCurrentColor: "#00ff00", //当前控制按钮的颜色
+    });
+    // 电子书轮播3
+    $('#ebooks-banner3').tyslide({
+        boxh: 223, //盒子的高度
+        w: 332, //盒子的宽度
+        h: 223, //图片的高度
+        isShow: true, //是否显示控制器
+        isShowBtn: true, //是否显示左右按钮
+        controltop: 10, //控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW: 20, //控制按钮宽度
+        controlsH: 3, //控制按钮高度
+        controlsColor: "#d7d7d7", //普通控制按钮的颜色
+        controlsCurrentColor: "#00ff00", //当前控制按钮的颜色
+    });
 
-    //电子书 table切换
-    var $lis = $()
 
     // 新书列表手风琴效果
     $('.ebooks .right-box ul > li').mouseenter(function() {
@@ -45,8 +69,50 @@ $(function() {
         $(this).find('.ebooks-title').hide(); //隐藏标题
         $(this).find('.desc').show(); //显示详情
     });
-    // 电子书轮播
+    //tab内容切换效果
+    $('.ebooks-list').eq(1).hide();
+    $('.ebooks-list').eq(2).hide();
+    var $lis = $('.ebooks .ebooks-nav > li');
+    $lis.mouseenter(function() {
+            $(this).addClass('active').siblings('li').removeClass('active');
+            var index = $(this).index();
+            var $ebookslist = $('.ebooks-list');
+            $ebookslist.eq(index).show().siblings('.ebooks-list').hide();
+        })
+        // 服装轮播
     $('#clothes-banner').tyslide({
+        boxh: 340, //盒子的高度
+        w: 430, //盒子的宽度
+        h: 340, //图片的高度
+        isShow: true, //是否显示控制器
+        isShowBtn: true, //是否显示左右按钮
+        controltop: 10, //控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW: 20, //控制按钮宽度
+        controlsH: 3, //控制按钮高度
+        controlsColor: "#d7d7d7", //普通控制按钮的颜色
+        controlsCurrentColor: "#00ff00", //当前控制按钮的颜色
+    });
+    //tab内容切换效果
+    $('.clothes .left-box .top .fr li').mouseenter(function() {
+            $(this).addClass('active').siblings().removeClass('active');
+            var index = $(this).index();
+            $('clothes .left-box  div').eq(index).show().removeClass().hide();
+        })
+        // 运动服装轮播
+    $('#sprot-banner').tyslide({
+        boxh: 340, //盒子的高度
+        w: 430, //盒子的宽度
+        h: 340, //图片的高度
+        isShow: true, //是否显示控制器
+        isShowBtn: true, //是否显示左右按钮
+        controltop: 10, //控制按钮上下偏移的位置,要将按钮向下移动   首先保证boxh 高度>图片 h
+        controlsW: 20, //控制按钮宽度
+        controlsH: 3, //控制按钮高度
+        controlsColor: "#d7d7d7", //普通控制按钮的颜色
+        controlsCurrentColor: "#00ff00", //当前控制按钮的颜色
+    });
+    // 儿童服装轮播
+    $('#children-clothes-banner').tyslide({
         boxh: 340, //盒子的高度
         w: 430, //盒子的宽度
         h: 340, //图片的高度
